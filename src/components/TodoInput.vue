f0927aee1474dcdfe4c099d891e148c99573cac1
BLUEHOLE\janakuma <janakuma@bluehole.net>
2021-11-09 21:20:54 +0900

    create todoinput

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <label for="todo-input"></label>
+        <input type="text" id="todo-input" />
+        <button @click="addTodo">add</button>
+    </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    addTodo() {
+      console.log("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
